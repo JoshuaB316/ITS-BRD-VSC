05f0298b3a7f9f10a4c81eac2a00d669e36d21f0
Mustafa Kocatürk <wyo840@haw-hamburg.de>
2025-10-23 17:01:39 +0200

    add all

--- Programs/Stack/tmp/Stack.Debug+ITSboard/tmp/Stack.Debug+ITSboard-mkdirs.cmake
+++ Programs/Stack/tmp/Stack.Debug+ITSboard/tmp/Stack.Debug+ITSboard-mkdirs.cmake
@@ -4,19 +4,19 @@
 cmake_minimum_required(VERSION 3.5)
 
 file(MAKE_DIRECTORY
-  "C:/GS_Beinert/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard"
-  "C:/GS_Beinert/ITS-BRD-VSC/Programs/Stack/tmp/1"
-  "C:/GS_Beinert/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard"
-  "C:/GS_Beinert/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/tmp"
-  "C:/GS_Beinert/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp"
-  "C:/GS_Beinert/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src"
-  "C:/GS_Beinert/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp"
+  "C:/GS_Labor/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard"
+  "C:/GS_Labor/ITS-BRD-VSC/Programs/Stack/tmp/1"
+  "C:/GS_Labor/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard"
+  "C:/GS_Labor/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/tmp"
+  "C:/GS_Labor/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp"
+  "C:/GS_Labor/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src"
+  "C:/GS_Labor/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp"
 )
 
 set(configSubDirs )
 foreach(subDir IN LISTS configSubDirs)
-    file(MAKE_DIRECTORY "C:/GS_Beinert/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/${subDir}")
+    file(MAKE_DIRECTORY "C:/GS_Labor/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/${subDir}")
 endforeach()
 if(cfgdir)
-  file(MAKE_DIRECTORY "C:/GS_Beinert/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp${cfgdir}") # cfgdir has leading slash
+  file(MAKE_DIRECTORY "C:/GS_Labor/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp${cfgdir}") # cfgdir has leading slash
 endif()
