a060a90f29ed3d36f7f221dc03dc7e1e880447ea
Mxsti43 <bumukoc@gmail.com>
2025-11-08 14:22:57 +0100

    new_calculate

--- Programs/GSP_Aufgabe1_Taschenrechner/Src/calculate.c
+++ Programs/GSP_Aufgabe1_Taschenrechner/Src/calculate.c
@@ -18,62 +18,96 @@
 #include "additionalFonts.h"
 #include "error.h"
 
+int safe_add(int a, int b, int *result) {
+    if ((b > 0) && (a > INT_MAX -b)) {
+        return 1; // Overflow
+    } else if ((b < 0) && (a < INT_MIN - b)) {
+        return 1; // Underflow
+    } else {
+        *result = a + b;
+        return 0; // is OK
+    }
+}
+
 extern void add() {
     int firstNum = pop();
     int secondNum = pop();
+    int result;
 
-    if (firstNum + secondNum > INT_MAX) {
-        lcdPrintlnS("Integer Overflow!\n");
-    }
-    else if(firstNum + secondNum < INT_MIN) {
-        lcdPrintlnS("Integer Underflow!\n");
-    }
-    else {
-        int result = firstNum + secondNum;
+    if (!safe_add(firstNum, secondNum, &result)){
+        lcdPrintlnS("Integer Overflow / Underflow!\n");
+    } else {
         push(result);
     }
 }
 
-void subtract() {
+int safe:substract(int a, int b, int *result) {
+    if ((b < 0) && (a > INT_MAX + b)) {
+        return 1; // Overflow
+    } else if ((b > 0) && (a < INT_MIN + b)) {
+        return 1; // Underflow
+    } else {
+        *result = a - b;
+        return 0; // is OK
+    }
+}
+
+extern void subtract() {
     int firstNum = pop();
     int secondNum = pop();
-    if(secondNum - secfirstNumondNum > INT_MAX){
-        lcdPrintlnS("Integer Overflow!\n");
-    }
-    else if(secondNum - firstNum < INT_MIN){
-        lcdPrintlnS("Integer Underflow!\n");
-    }
-    else  {
-        int result = secondNum - firstNum;
+    int result;
+
+    if (!safe_substract(secondNum, firstNum, &result)){
+        lcdPrintlnS("Integer Overflow / Underflow!\n");
+    } else {
         push(result);
     }
 }
 
-void multiply() {
+int safe_multiply(int a, int b, int *result) {
+    if (((a > 0) && (b > 0) && (a > INT_MAX / b)) ||
+       ((a < 0) && (b < 0) && (a < INT_MAX / b))) {
+        return 1; // Overflow
+    } else if (((a > 0) && (b < 0) && (b < INT_MIN / a)) ||
+               ((a < 0) && (b > 0) && (a < INT_MIN / b))) {
+        return 1; // Underflow
+    } else {
+        *result = a * b;
+        return 0; // is OK
+    }
+}
+
+extern void multiply() {
     int firstNum = pop();
     int secondNum = pop();
-    if(firstNum * secondNum > INT_MAX){
-        lcdPrintlnS("Integer Overflow!\n");
-    }
-    else if(firstNum * secondNum < INT_MIN){
-        lcdPrintlnS("Integer Underflow!\n");
-    }
-    else  {
-        int result = firstNum * secondNum;
+    int result;
+
+    if (!safe_multiply(firstNum, secondNum, &result)){
+        lcdPrintlnS("Integer Overflow / Underflow!\n");
+    } else {
         push(result);
     }
 }
 
-void divide() {
+int safe_divide(int a, int b, int *result) {
+    if (b == 0) {
+        return 1; // Division by zero
+    } else if ((a == INT_MIN) && (b == -1)) {
+        return 1; // Overflow
+    } else {
+        *result = a / b;
+        return 0; // is OK
+    }
+}
+
+extern void divide() {
     int firstNum = pop();
     int secondNum = pop();
-    if (firstNum != 0) {
-        int result = secondNum / firstNum;
+    int result;
+
+    if (!safe_divide(secondNum, firstNum, &result)){
+        lcdPrintlnS("Error: Division by zero or Integer Overflow!\n");
+    } else {
         push(result);
-    }
-   else {
-        lcdPrintlnS("cannot divide by 0\n");
-        push(secondNum);
-        push(firstNum);
     }
 }
