27b200e71c0ce2d0996d07267f5cc6437b2d222c
Joshua Beinert <wxr316@haw-hamburg.de>
2025-11-30 20:55:40 +0100

    errorhaendler2

---
+++ Programs/GSA1/Inc/errorhaendler.h
@@ -0,0 +1,24 @@
+/**
+  * @file errorhaendler.h
+  * @author MJ 
+  * @date Oct 2025
+  */
+
+#ifndef ERRORHAENDLER_H
+#define ERRORHAENDLER_H
+
+#define StackOverflow -1
+#define StackUnderflow -2
+#define IntOverflow -3
+#define IntUnderflow -4
+#define DivideByZero -5
+
+/**
+ * @brief displays errormessage based on the error
+ * 
+ * @param errNum predefined integer values
+ * @return void
+ */
+void displayError(int errNum)
+
+#endif
