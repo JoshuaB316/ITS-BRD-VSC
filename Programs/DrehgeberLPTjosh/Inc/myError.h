70af9e81d783de56e43818d3237801dfb6d92328
JoshuaB316 <wxr316@haw-hamburg.de>
2026-05-28 13:06:02 +0200

    random

---
+++ Programs/DrehgeberLPTjosh/Inc/myError.h
@@ -0,0 +1,15 @@
+/**
+ * @file    myError.h
+ * @authors Mustafa Kocatürk, Joshua Beinert
+ * @date    Mai 2026
+ */
+
+#ifndef MYERROR_H
+#define MYERROR_H
+
+/**
+ * @brief hier werden alle Variablen zurückgesetzt
+ */
+void errorReset();
+
+#endif
